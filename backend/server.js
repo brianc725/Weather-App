5b13202c815e947d97fb9e2e9fafe8ac387a8045
Brian Chan <brianc725@gmail.com>
2020-04-17 23:24:33 -0700

    Connect backend with redis instance with docker compose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const express = require('express');
+const Redis = require("ioredis");
+
+const redis = new Redis({ host: 'redis' });
 
 // Constants
 const PORT = 8080;
@@ -8,6 +11,31 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+app.get('/set', (req, res) => {
+  redis.set("foo", "bar", function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.get('/get', (req, res) => {
+  redis.get("foo", function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      if (result === null) {
+        res.send('No data');
+      } else {
+        res.send(result);
+      }
+    }
+  });
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
