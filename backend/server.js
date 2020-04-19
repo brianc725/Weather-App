ee8227f0828da6a56f8dc6e2523200501c6d6ecd
Brian Chan <brianc725@gmail.com>
2020-04-19 00:57:42 -0700

    Write the backend out for current weather api call

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@
 
 const express = require('express');
 const Redis = require("ioredis");
+const fetch = require('node-fetch');
+var moment = require('moment');
+
+require('dotenv').config()
 
 const redis = new Redis({ host: 'redis' });
 
@@ -12,32 +16,50 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 
-app.get('/set', (req, res) => {
-  redis.set("foo", "bar", function (err, result) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
+async function getWeather(req, res) {
+  const { zipCode, units } = req.params;
+  const { WEATHER_API_KEY } = process.env;
 
-app.get('/get', (req, res) => {
-  redis.get("foo", function (err, result) {
-    if (err) {
-      res.send(err);
+  const key = zipCode + units;
+
+  const weatherURL = `http://api.openweathermap.org/data/2.5/weather?appid=${WEATHER_API_KEY}&zip=${zipCode}&units=${units}`;
+  await fetch(weatherURL)
+    .then((response) => response.json())
+    .then((data) => {
+      const result = JSON.stringify(data);
+
+      // Store in redis with expiration of seconds until midnight
+      const secondsTillMidnight = moment("24:00:00", "hh:mm:ss").diff(moment(), 'seconds');
+      redis.set(key, result, "EX", secondsTillMidnight);
+
+      res.send(result);
+    })
+    .catch((error) => {
+      res.status(500).send(JSON.stringify(error.toString()));
+    });
+}
+
+function redisCacher(req, res, next) {
+  const { zipCode, units } = req.params;
+
+  const key = zipCode + units;
+
+  redis.get(key, (err, value) => {
+    if (err) throw err;
+
+    if (value !== null) {
+      // Value is already JSON stringified data from before
+      res.send(value);
     } else {
-      if (result === null) {
-        res.send('No data');
-      } else {
-        res.send(result);
-      }
+      next();
     }
   });
-});
+}
+
+app.get('/api/weather/:zipCode/:units', redisCacher, getWeather)
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Weather Application API');
 });
 
 app.listen(PORT, HOST);
